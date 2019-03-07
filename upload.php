d64c6268808b01502d40acb8f198abdcf4bf1a34
Diogodhs <dhs@cin.ufpe.br>
2019-03-06 21:27:05 -0300

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+$pasta = "./crachas";
+/* Coloque aqui,
+a pasta no servidor onde os arquivos serão salvos.
+Atenção: se você não
+souber sua pasta no servidor, contate o administrador
+do mesmo. */
+
+$dest = $pasta."/"."cracha"; // Não altere esta variável.
+
+if(!move_uploaded_file($file, $dest)) { // Executa o comando do upload no servidor
+   echo "Não foi possível enviar o arquivo!"; /* Caso não foi possível enviar o arquivo, mostra o erro. */
+} else {
+   echo "Arquivo enviado com sucesso!";
+/* Caso o arquivo tenha sido enviado
+   com sucesso, mostra a mensagem de
+sucesso. */
+}
+?>
